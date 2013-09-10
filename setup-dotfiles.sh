bfb9de185ae87bf34d112999af07e60607b32ec4
Luther P <post2base@outlook.com>
2013-09-10 17:07:57 +0100

    First commit.

---
+++ setup-dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Simple setup-dotfiles.sh for pulling and configuring dotfiles
+
+# git pull and install dotfiles
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+if [ -d .tmux/ ]; then
+    mv .tmux .tmux~
+fi
+
+cp -r setup-cygwin/dotfiles .
+
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.tmux.conf .
+ln -sb dotfiles/.gitmessage.txt .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+ln -sf dotfiles/.tmux .
+
---
+++ setup.sh
@@ -0,0 +1,66 @@
+B77;10103;0c#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+sudo npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+#sudo apt-get install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+#sudo add-apt-repository -y ppa:cassou/emacs
+#sudo apt-get -qq update
+#sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+#Install tmux
+#sudo apt-get install -y tmux
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+#Install restler, cheerio and commander
+npm install restler
+npm install commander
+npm install cheerio 
+npm install express
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+if [ -d .tmux/ ]; then
+    mv .tmux .tmux~
+fi
+
+#Copy setup dotfiles into home directory
+cp -r setup-cygwin/dotfiles .
+
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.tmux.conf .
+ln -sb dotfiles/.gitmessage.txt .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+ln -sf dotfiles/.tmux .
+
