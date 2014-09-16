d78bd5961e5d34d79917ea687df1aa3d84be7724
Quantza <post2base@outlook.com>
2014-09-16 16:30:33 +0100

    Updated setup scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# Simple setup.sh for configuring Ubuntu 14.04 LTS EC2 instance
 # for headless setup. 
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.0/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.18
-nvm use v0.10.18
+nvm install v0.10
+nvm use v0.10
+
+#Set node version for new shells
+nvm alias default 0.10
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -23,9 +26,10 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+#sudo add-apt-repository -y ppa:cassou/emacs
+#sudo apt-get -qq update
+#sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y emacs24
 
 #Install tmux
 sudo apt-get install -y tmux
@@ -34,26 +38,38 @@ sudo apt-get install -y tmux
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-#Install restler, cheerio and commander
+#Install restler, cheerio and commander for node.js
+
+#Rest client
 npm install restler
+
+#cmdline in js
 npm install commander
-npm install cheerio 
-npm install express
+
+#To use jQuery on a server - windows-friendly
+npm install cheerio
+
+#Web app framework
+npm install -g express
 
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
+
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
+
 if [ -d .tmux/ ]; then
     mv .tmux .tmux~
 fi
+
 if [ -d .vagrant.d/ ]; then
     mv .vagrant.d .vagrant.d~
 fi
+
 if [ -d .tools/ ]; then
     mv .tools .tools.old
 fi
--- start-agent.sh
+++ start-agent.sh
@@ -1,5 +1,5 @@
 #! /bin/bash 
-eval `ssh-agent -s` 
-ssh-add $HOME/.ssh/id_rsa_g
-ssh-add $HOME/.ssh/private1.key
-ssh-add $HOME/.ssh/private2.key
+eval "$(ssh-agent -s)"
+ssh-add $HOME/.ssh/web-dev
+ssh-add $HOME/.ssh/gHub_rsa
+ssh-add $HOME/.ssh/ko-dev
