64df1a5a2ea72b0d3c5f28aee0546e8cdf08a935
Quantza <post2base@outlook.com>
2016-02-13 12:19:47 +0000

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+echo "Test"
+
 #!/bin/bash
 # Simple setup.sh for configuring Ubuntu 14.04 and derivatives,
 # for headless setup.
@@ -5,12 +7,12 @@
 # Install apt-cyg
 # https://github.com/pi0/cyg
 
+echo "Test"
+
 GIT_REPOS_DIR="$HOME/GitRepos"
 if [ ! -d $GIT_REPOS_DIR ]; then
     mkdir $GIT_REPOS_DIR
 fi
-
-echo "Test"
 
 CYG_REPO_NAME="cyg"
 CYG_REPO_DIR="$GIT_REPOS_DIR/$CYG_REPO_NAME"
