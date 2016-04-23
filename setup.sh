7cb04b4c91c6f624cbfd8d6632e1437b348bfad3
Quantza <post2base@outlook.com>
2016-04-23 18:30:50 +0100

    Fixing scripts

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 # Simple setup.sh for configuring cygwin,
 # for headless setup.
 
-OLDDIR="$PWD"
+OLDBASEDIR="$PWD"
 
 touch "$HOME"/"cygwin-trigger"
 
 chmod a+x ./setup_init.sh
 source ./setup_init.sh
 
-cd "$OLDDIR"
+cd "$OLDBASEDIR"
 
 chmod a+x ./setup_version_managers.sh
 source 	./setup_version_managers.sh
 
-cd "$OLDDIR"
+cd "$OLDBASEDIR"
 
 chmod a+x ./setup_helper.sh
 /bin/bash ./setup_helper.sh
 
-cd "$OLDDIR"
+cd "$OLDBASEDIR"
 
